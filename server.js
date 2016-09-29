5af854a922526384a153c9b1e4798c9143d269b1
Shubham Agarwal <shubhamal1995@gmail.com>
2016-09-29 18:44:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function (req, res) {
+  res.send('article one requested and is served here');
+});
+
+app.get('/article-two', function (req, res) {
+  res.send('article two requested and is served here');
+});
+
+app.get('/article-three', function (req, res) {
+  res.send('article three requested and is served here');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
